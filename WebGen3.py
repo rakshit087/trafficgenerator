dfa39065cdd6ee2b3f6c14ad2e10830e29f6e5d7
rakshit087 <rakshit087@gmail.com>
2020-06-30 17:10:53 +0530

    Create WebGen3.py

---
+++ WebGen3.py
@@ -0,0 +1,49 @@
+import sys
+import random
+import time
+from stem import Signal
+from stem.control import Controller
+import requests
+from fake_useragent import UserAgent
+
+address = input("Enter the Web Address of your Site")
+views = int(input("\033[92m How many views you want: \033[00m"))
+
+
+def renew_connection():
+    with Controller.from_port(address="127.0.0.1", port=9051) as controller:
+        controller.authenticate(password="")
+        controller.signal(Signal.NEWNYM)
+        controller.signal(Signal.HUP)
+        time.sleep(2)
+
+
+def tor_session():
+    session = requests.session()
+    session.proxies = {}
+    session.proxies['http'] = 'socks5://localhost:9050'
+    session.proxies['https'] = 'socks5://localhost:9050'
+    return session
+
+
+def visit():
+
+    for num in range(views):
+        ua = UserAgent()
+        header = {}
+        header['User-Agent'] = ua.random
+        session = tor_session()
+        session.get(address, headers=header)
+        time.sleep(10)
+        print(num)
+        if (num) % 10 ==0:
+            renew_connection()
+
+        if num == (views - 1):
+            print("Done")
+            sys.exit(0)
+
+
+visit()
+
+
